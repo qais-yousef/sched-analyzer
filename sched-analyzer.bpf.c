95ceed4d1808e53694bbefabd72de8138719d043
Qais Yousef <qyousef@layalina.io>
2022-04-18 03:23:28 +0100

    bpf-analyzer.bpf.c: Print uclamp_min and uclamp_max info
    Getting more 'sophisticated' slowly :-)
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- sched-analyzer.bpf.c
+++ sched-analyzer.bpf.c
@@ -8,9 +8,13 @@ char LICENSE[] SEC("license") = "GPL";
 SEC("raw_tp/pelt_se_tp")
 int BPF_PROG(handle_pelt_se, struct sched_entity *se)
 {
-	int cpu = BPF_CORE_READ(se, cfs_rq, rq, cpu);
+	struct rq *rq = BPF_CORE_READ(se, cfs_rq, rq);
+	int cpu = BPF_CORE_READ(rq, cpu);
+	unsigned long uclamp_min = BPF_CORE_READ(rq, uclamp[UCLAMP_MIN].value);
+	unsigned long uclamp_max = BPF_CORE_READ(rq, uclamp[UCLAMP_MAX].value);
 
-	bpf_printk("[%d] Hello world!", cpu);
+	bpf_printk("[%d] uclamp_min = %lu uclamp_max = %lu",
+		   cpu, uclamp_min, uclamp_max);
 
 	return 0;
 }
