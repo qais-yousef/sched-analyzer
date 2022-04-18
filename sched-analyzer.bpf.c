fdb40b9a02e91dd6a85095a8525809a6131412b9
Qais Yousef <qyousef@layalina.io>
2022-04-18 04:59:57 +0100

    bpf-analyzer.bpf.c: Extract more uclamp info
    There's a weird issue where we must read 'comm' after reading uclamp
    values or it'd be NULL..
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- sched-analyzer.bpf.c
+++ sched-analyzer.bpf.c
@@ -5,16 +5,43 @@
 
 char LICENSE[] SEC("license") = "GPL";
 
+static inline bool entity_is_task(struct sched_entity *se)
+{
+	if (bpf_core_field_exists(se->my_q))
+		return !BPF_CORE_READ(se, my_q);
+	else
+		return true;
+}
+
 SEC("raw_tp/pelt_se_tp")
 int BPF_PROG(handle_pelt_se, struct sched_entity *se)
 {
-	struct rq *rq = BPF_CORE_READ(se, cfs_rq, rq);
-	int cpu = BPF_CORE_READ(rq, cpu);
-	unsigned long uclamp_min = BPF_CORE_READ(rq, uclamp[UCLAMP_MIN].value);
-	unsigned long uclamp_max = BPF_CORE_READ(rq, uclamp[UCLAMP_MAX].value);
 
-	bpf_printk("[%d] uclamp_min = %lu uclamp_max = %lu",
-		   cpu, uclamp_min, uclamp_max);
+	if (entity_is_task(se)) {
+		struct task_struct *p = container_of(se, struct task_struct, se);
+		unsigned long uclamp_min, uclamp_max;
+		char *comm;
+
+		uclamp_min = BPF_CORE_READ_BITFIELD_PROBED(p, uclamp_req[UCLAMP_MIN].value);
+		uclamp_max = BPF_CORE_READ_BITFIELD_PROBED(p, uclamp_req[UCLAMP_MAX].value);
+		comm = BPF_CORE_READ(p, comm);
+		bpf_printk("[%s] Req: uclamp_min = %lu uclamp_max = %lu",
+			   comm, uclamp_min, uclamp_max);
+
+		uclamp_min = BPF_CORE_READ_BITFIELD_PROBED(p, uclamp[UCLAMP_MIN].value);
+		uclamp_max = BPF_CORE_READ_BITFIELD_PROBED(p, uclamp[UCLAMP_MAX].value);
+		comm = BPF_CORE_READ(p, comm);
+		bpf_printk("[%s] Eff: uclamp_min = %lu uclamp_max = %lu",
+			   comm, uclamp_min, uclamp_max);
+	} else {
+		struct rq *rq = BPF_CORE_READ(se, cfs_rq, rq);
+		int cpu = BPF_CORE_READ(rq, cpu);
+		unsigned long uclamp_min = BPF_CORE_READ(rq, uclamp[UCLAMP_MIN].value);
+		unsigned long uclamp_max = BPF_CORE_READ(rq, uclamp[UCLAMP_MAX].value);
+
+		bpf_printk("[CPU%d] uclamp_min = %lu uclamp_max = %lu",
+			   cpu, uclamp_min, uclamp_max);
+	}
 
 	return 0;
 }
