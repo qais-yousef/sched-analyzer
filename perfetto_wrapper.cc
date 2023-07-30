346d85fe368626140adbbaab0ebecf8e012deef6
Qais Yousef <qyousef@layalina.io>
2023-07-30 01:21:04 +0100

    perfetto: Switch to system mode by default
    The trick to make it work is to start:
    
    	sudo tracebox traced --background
    	sudo tracebox traced_porbes --background
    
    then whenever you want run:
    
    	sudo ./sched-analyzer
    
    and you'll find sched-analyzer.perfetto-trace in $PWD.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- perfetto_wrapper.cc
+++ perfetto_wrapper.cc
@@ -52,13 +52,13 @@ extern "C" void init_perfetto(void)
 	// or more of:
 
 	// 1) The in-process backend only records within the app itself.
-	args.backends |= perfetto::kInProcessBackend;
+	/* args.backends |= perfetto::kInProcessBackend; */
 
 	// 2) The system backend writes events into a system Perfetto daemon,
 	//    allowing merging app and system events (e.g., ftrace) on the same
 	//    timeline. Requires the Perfetto `traced` daemon to be running (e.g.,
 	//    on Android Pie and newer).
-	/* args.backends |= perfetto::kSystemBackend; */
+	args.backends |= perfetto::kSystemBackend;
 
 	perfetto::Tracing::Initialize(args);
 	perfetto::TrackEvent::Register();
