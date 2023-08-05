83cbfd9c23b3b341dd043d7447251d4b18447beb
Qais Yousef <qyousef@layalina.io>
2023-08-05 23:52:29 +0100

    cross_compile: Fix wrong names in guess_cross_compile function
    We convert aarch64 to arm64 etc. So use the converted names.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- cross_compile.mk
+++ cross_compile.mk
@@ -6,10 +6,10 @@ guess_arch := $(shell uname -m			| \
 		)
 
 guess_cross_compile = $(shell echo $(1)			| \
-		sed 's/x86_64/x86_64-linux-gnu-/'	| \
-		sed 's/aarch64/aarch64-linux-gnu-/'	| \
-		sed 's/ppc64le/powerpc64le-linux-gnu-/'	| \
-		sed 's/mips.*/mips-linux-gnu-/'		  \
+		sed 's/x86/x86_64-linux-gnu-/'	| \
+		sed 's/arm64/aarch64-linux-gnu-/'	| \
+		sed 's/powerpc/powerpc64le-linux-gnu-/'	| \
+		sed 's/mips/mips-linux-gnu-/'		  \
 		)
 
 ARCH ?= $(call guess_arch)
