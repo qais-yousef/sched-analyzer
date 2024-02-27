ad787566b926911e1dc4b33be53e23c743ba6fb9
Qais Yousef <qyousef@layalina.io>
2024-02-27 00:03:54 +0000

    sched-analyzer-pp: Generalize pelt to enable capturing other signals
    It was hard coded to util_avg. But now we should be able more easily
    to add options to plot the rest of the signals.
    
    Signed-off-by: Qais Yousef <qyousef@layalina.io>

--- sched-analyzer-pp/pelt.py
+++ sched-analyzer-pp/pelt.py
@@ -8,32 +8,33 @@ query = "select c.ts as ts, c.value as value, t.name as counter_name \
         left join process as p using (upid) \
         where p.name like '%sched-analyzer' and counter_name like '{}'"
 
-query_util_avg = query.format('% util_avg')
 
+def init(trace, signal):
 
-def init(trace):
+    global pelt_signal
+    pelt_signal = signal
 
-    global trace_util_avg
-    trace_util_avg = trace.query(query_util_avg)
+    global trace_pelt
+    trace_pelt = trace.query(query.format('% {}'.format(signal)))
 
-    global df_util_avg
-    df_util_avg = trace_util_avg.as_pandas_dataframe()
-    df_util_avg.ts = df_util_avg.ts - df_util_avg.ts[0]
-    df_util_avg.ts = df_util_avg.ts / 1000000000
-    df_util_avg.set_index('ts', inplace=True)
+    global df_pelt
+    df_pelt = trace_pelt.as_pandas_dataframe()
+    df_pelt.ts = df_pelt.ts - df_pelt.ts[0]
+    df_pelt.ts = df_pelt.ts / 1000000000
+    df_pelt.set_index('ts', inplace=True)
 
-def util_avg_save_csv(prefix):
+def pelt_save_csv(prefix):
 
-    df_util_avg.to_csv(prefix + '_util_avg.csv')
+    df_pelt.to_csv(prefix + '_' + pelt_signal + '.csv')
 
-def plot_util_avg_matplotlib(plt, prefix, tracks=[]):
+def plot_pelt_matplotlib(plt, prefix, tracks=[]):
 
     if not any(tracks):
-        tracks = df_util_avg.counter_name.unique()
+        tracks = df_pelt.counter_name.unique()
 
     num_rows = 0
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         num_rows += len(subtracks)
     row_pos = 1
 
@@ -43,10 +44,10 @@ def plot_util_avg_matplotlib(plt, prefix, tracks=[]):
     plt.figure(figsize=(8,4*num_rows))
 
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         subtracks = sorted(subtracks)
         for track in subtracks:
-            df = df_util_avg[df_util_avg.counter_name == track]
+            df = df_pelt[df_pelt.counter_name == track]
 
             plt.subplot(num_rows, 1, row_pos)
             row_pos += 1
@@ -55,16 +56,16 @@ def plot_util_avg_matplotlib(plt, prefix, tracks=[]):
             plt.grid()
 
     plt.tight_layout()
-    plt.savefig(prefix + '_util_avg.png')
+    plt.savefig(prefix + '_' + pelt_signal + '.png')
 
-def plot_util_avg_hist_matplotlib(plt, prefix, tracks=[]):
+def plot_pelt_hist_matplotlib(plt, prefix, tracks=[]):
 
     if not any(tracks):
-        tracks = df_util_avg.counter_name.unique()
+        tracks = df_pelt.counter_name.unique()
 
     num_rows = 0
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         num_rows += len(subtracks)
     row_pos = 1
 
@@ -74,10 +75,10 @@ def plot_util_avg_hist_matplotlib(plt, prefix, tracks=[]):
     plt.figure(figsize=(8,4*num_rows))
 
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         subtracks = sorted(subtracks)
         for track in subtracks:
-            df = df_util_avg[df_util_avg.counter_name == track]
+            df = df_pelt[df_pelt.counter_name == track]
 
             plt.subplot(num_rows, 1, row_pos)
             row_pos += 1
@@ -85,40 +86,40 @@ def plot_util_avg_hist_matplotlib(plt, prefix, tracks=[]):
             df.value.hist(bins=100, density=False, grid=True, alpha=0.5, legend=True)
 
     plt.tight_layout()
-    plt.savefig(prefix + '_util_avg_hist.png')
+    plt.savefig(prefix + '_' + pelt_signal + '_hist.png')
 
-def plot_util_avg_tui(plt, tracks=[]):
+def plot_pelt_tui(plt, tracks=[]):
 
     if not any(tracks):
-        tracks = df_util_avg.counter_name.unique()
+        tracks = df_pelt.counter_name.unique()
 
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         subtracks = sorted(subtracks)
         for track in subtracks:
-            df = df_util_avg[df_util_avg.counter_name == track]
+            df = df_pelt[df_pelt.counter_name == track]
 
             plt.cld()
             plt.plot_size(100, 10)
             plt.plot(df.index.values, df.value.values)
-            plt.title(track + ' util_avg')
+            plt.title(track + ' ' + pelt_signal)
             plt.show()
 
-def plot_util_avg_hist_tui(plt, tracks=[]):
+def plot_pelt_hist_tui(plt, tracks=[]):
 
     if not any(tracks):
-        tracks = df_util_avg.counter_name.unique()
+        tracks = df_pelt.counter_name.unique()
 
     for track in tracks:
-        subtracks =  df_util_avg[df_util_avg.counter_name.str.contains(track)].counter_name.unique()
+        subtracks =  df_pelt[df_pelt.counter_name.str.contains(track)].counter_name.unique()
         subtracks = sorted(subtracks)
         for track in subtracks:
-            df = df_util_avg[df_util_avg.counter_name == track]
+            df = df_pelt[df_pelt.counter_name == track]
 
             df_hist = pd.Series(df.value.value_counts(ascending=True))
 
             plt.cld()
             plt.plot_size(100, 10)
             plt.bar(df_hist.index, df_hist.values, width=1/5)
-            plt.title(track + ' util_avg Histogram')
+            plt.title(track + ' ' + pelt_signal + ' Histogram')
             plt.show()
